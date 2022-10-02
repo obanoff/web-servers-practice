4a7d420f84b8e2778118f649909946561482627f
obanoff <obanoff@gmail.com>
2022-10-02 12:05:44 +0300

    added marco

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 
-fn main() {
-    mode("multi threaded");
-}
-
-
-fn mode(m: &str) {
+fn mode(m: &str, threads: usize) {
     match m {
         "multi threaded" => {
             use web_server::multi_threaded;
 
-            multi_threaded::run(4);
+            multi_threaded::run(threads);
         },
         "async" => {
             println!("Missing implementation");
         },
         _ => (),
     }
+}
+
+macro_rules! mode {
+    ($x: expr) => {
+        mode($x, 4) // in a case of only the first argument provided, set the second argument to 4 (default parameter)
+    };
+    ($x: expr, $y: expr) => {
+        mode($x, $y) // a case of both arguments provided
+    };
+}
+
+fn main() {
+    mode!("multi threaded");
 }
 
 
